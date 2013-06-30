fa207ed5db296801e306a25d24a3f144d61d96a5
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-30 12:11:51 -0500

    adds styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
+  before_filter :authenticate_user!, except: [:home]
+
   def home
   end
 
+  def index
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Typing::Application.routes.draw do
   
   devise_for :users
-
   root :to => "users#home"
 
+  resources :users, :only => [:index]
 
 end
