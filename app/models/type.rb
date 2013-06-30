4cfb5b9cdc19faadb7af679fd830dd43327df77a
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-30 13:19:26 -0500

    fixes typo on typing migration

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,4 +1,4 @@
 class Type < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :content
   belongs_to :user
 end
--- db/migrate/20130630180958_create_types.rb
+++ db/migrate/20130630180958_create_types.rb
@@ -2,7 +2,7 @@ class CreateTypes < ActiveRecord::Migration
   def change
     create_table :types do |t|
       t.text  :content
-      t.ingeger :user_id
+      t.integer :user_id
       t.timestamps
     end
   end
